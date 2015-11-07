9a215a2c4ed232b47b109dd0ed1bfb9150479660
Jhon Edison Ariza <jhon_ariza03@hotmail.com>
2015-11-07 12:58:43 -0500

    acelerometro
    metodos

--- modulo-8/lab1/platforms/android/assets/www/js/lab1.js
+++ modulo-8/lab1/platforms/android/assets/www/js/lab1.js
@@ -1,8 +1,9 @@
 angular.module('lab1', ['ngCordova'])
 
 .controller('lab1Controller', function($scope, $cordovaDeviceMotion){
-
+    var options = { frequency: 2000 };
     document.addEventListener("deviceready", function () {
+
 
         $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
             $scope.x = result.x;
@@ -14,5 +15,33 @@ angular.module('lab1', ['ngCordova'])
             console.log(err);
         });
 
+
+
+    var watch = $cordovaDeviceMotion.watchAcceleration(options);
+    watch.then(
+      null,
+      function(error) {
+      // An error occurred
+      },
+      function(result) {
+         $scope.x = result.x;
+            $scope.y = result.y;
+            $scope.z = result.z;
+            $scope.time_stamp = result.timestamp;
+    });
+
     }, false);
+
+    $scope.runDeviceMotion = function()
+    {
+         $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+            $scope.x = result.x;
+            $scope.y = result.y;
+            $scope.z = result.z;
+            $scope.time_stamp = result.timestamp;
+        }, function(err) {
+            // An error occurred. Show a message to the user
+            console.log(err);
+        });
+    }
 });
